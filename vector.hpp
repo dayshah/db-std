fe6ca5a7dfa3c6a0b518e959954aec3725e1581d
dayshah <dhyeyhshah@gmail.com>
2024-07-06 17:14:10 -0400

    a working vector implementation

---
+++ vector.hpp
@@ -0,0 +1,125 @@
+#ifndef DBSTD_VECTOR
+#define DBSTD_VECTOR
+
+#include <cstdlib>
+#include <stddef.h>
+#include <algorithm>
+
+namespace dbstd {
+
+template <typename T>
+class Vector {
+
+private:
+    size_t capacity;
+    size_t size;
+    T* backing;
+
+    T* createBacking(size_t capacity) {
+        return static_cast<T*>(std::malloc(capacity*sizeof(T)));
+    }
+
+    void freeBacking(T* toFree, size_t size) {
+        for (T* location = toFree; location < (toFree+size); ++location)
+            location->~T();
+        std::free(static_cast<void*>(toFree));
+    }
+
+    void copyBacking(T* inputStart, T* inputEnd, T* outputStart) {
+        for (; inputStart < inputEnd; ++inputStart, ++outputStart)
+            new (outputStart) T(*inputStart);
+    }
+
+public:
+    Vector()
+    : capacity(0)
+    , size(0)
+    , backing(createBacking(capacity))
+    {}
+
+    Vector(size_t capacity)
+    : capacity(capacity)
+    , size(0)
+    , backing(createBacking(capacity))
+    {}
+
+    Vector(const Vector& other)
+    : capacity(other.capacity)
+    , size(other.size)
+    , backing(createBacking(capacity))
+    {
+        copyBacking(other.backing, other.backing+size, backing); 
+    }
+
+    Vector(Vector&& other) noexcept
+    : capacity(other.capacity)
+    , size(other.size)
+    , backing(other.backing)
+    {
+        other.backing = nullptr;
+    }
+
+    // Copy swap example
+    // friend void swap(Vector first, Vector second) {
+    //     std::swap(first.capacity, second.capacity);
+    //     std::swap(first.size, second.size);
+    //     std::swap(first.backing, second.backing);
+    // }
+    // Vector& operator=(Vector other) noexcept {
+    //     swap(*this, other);
+    //     return *this;
+    // }
+
+    Vector& operator=(const Vector& other) {
+        if (this != &other) {
+            freeBacking(backing, size);
+            capacity = other.capacity;
+            size = other.size;
+            backing = createBacking(capacity);
+            copyBacking(other.backing, other.backing + other.size, backing);
+        }
+        return *this;
+    }
+
+    Vector& operator=(Vector&& other) noexcept {
+        if (this != &other) {
+            freeBacking(backing, size);
+            capacity = other.capacity;
+            size = other.size;
+            backing = other.backing;
+            other.backing = nullptr;
+        }
+        return *this;
+    }
+
+    ~Vector() {
+        if (backing != nullptr) freeBacking(backing, size);
+    }
+
+    void reserve(size_t newCapacity) {
+        capacity = newCapacity;
+        T* newBacking = createBacking(capacity);
+        copyBacking(backing, backing+size, newBacking);
+        freeBacking(backing, size);
+        backing = newBacking;
+    }
+
+    template <typename U>
+    void push_back(U&& item) {
+        if (size == capacity) {
+            capacity == 0
+                ? reserve(1)
+                : reserve(capacity*2);
+        }
+        new (backing+size) T(std::forward<U>(item));
+        ++size;
+    }
+
+    T& operator[](size_t index) {
+        return *(backing + index);
+    }
+};
+
+}
+
+#endif
---
+++ vector.t.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <ostream>
+
+#include "vector.hpp"
+#include "lifetime.hpp"
+
+int main() {
+    using namespace dbstd;
+
+    Vector<int> intVec{2};
+    intVec.push_back(4);
+    intVec.push_back(2);
+
+    Vector<Lifetime> lfVec;
+    lfVec.push_back(Lifetime{"moved lf"});
+    const Lifetime toCopy{"copied lf"};
+    lfVec.push_back(toCopy);
+
+    std::cout << "\nfirst vec contents************\n";
+    std::cout << lfVec[0] << '\n';
+    std::cout << lfVec[1] << '\n';
+    std::cout << "**************************\n\n";
+
+    Vector<Lifetime> copyConstructed{lfVec};
+    std::cout << "\ncopy constructed contents************\n";
+    std::cout << copyConstructed[0] << '\n';
+    std::cout << copyConstructed[1] << '\n';
+    std::cout << "**************************\n\n";
+
+    Vector<Lifetime> moveConstructed{std::move(lfVec)};
+    std::cout << "\nmove constructed contents************\n";
+    std::cout << moveConstructed[0] << '\n';
+    std::cout << moveConstructed[1] << '\n';
+    std::cout << "**************************\n\n";
+
+    copyConstructed = moveConstructed;
+    std::cout << "\ncopy assigned contents************\n";
+    std::cout << copyConstructed[0] << '\n';
+    std::cout << copyConstructed[1] << '\n';
+    std::cout << "**************************\n\n";
+
+    moveConstructed = std::move(copyConstructed);
+    std::cout << "\nmove assigned contents************\n";
+    std::cout << moveConstructed[0] << '\n';
+    std::cout << moveConstructed[1] << '\n';
+    std::cout << "**************************\n\n";
+
+
+    return 0;
+}
