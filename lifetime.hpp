3b797b33f29e3876b6f6b7c123f01479e52d3e79
dayshah <dhyeyhshah@gmail.com>
2024-07-07 13:39:39 -0400

    managed lifetime

--- lifetime.hpp
+++ lifetime.hpp
@@ -7,43 +7,49 @@
 namespace dbstd {
 
 struct Lifetime {
-    std::string data;
+    std::string* data;
 
-    Lifetime(std::string_view data): data(data)
+    Lifetime(std::string_view data): data(new std::string{data})
     { std::cout << "constructing" << std::endl; }
 
     Lifetime(const Lifetime& other)
-    : data(other.data)
+    : data(new std::string(*other.data))
     { std::cout << "copying" << std::endl; }
 
     Lifetime(Lifetime&& other) noexcept
-    : data(std::move(other.data))
-    { std::cout << "moving" << std::endl; }
-
-    friend void swap(Lifetime& first, Lifetime& second) {
-        std::swap(first.data, second.data);
+    : data(other.data)
+    {
+        other.data = nullptr;
+        std::cout << "moving" << std::endl;
     }
 
     Lifetime& operator=(const Lifetime& other) {
         std::cout << "copy assigning" << std::endl;
-        if (this == &other) return *this;
-        auto otherCopy = other;
-        swap(*this, otherCopy);
+        if (this != &other) {
+            delete this->data;
+            this->data = new std::string(*other.data);
+        }
         return *this;
     }
 
     Lifetime& operator=(Lifetime&& other) noexcept {
         std::cout << "move assigning" << std::endl;
-        if (this == &other) return *this;
-        swap(*this, other);
+        if (this != &other) {
+            delete this->data;
+            this->data = other.data;
+            other.data = nullptr;
+        }
         return *this;
     }
 
     ~Lifetime() 
-    { std::cout << "destructing" << std::endl; }
+    {
+        std::cout << "destructing" << std::endl;
+        delete this->data;
+    }
 
     friend std::ostream& operator<<(std::ostream& out, const Lifetime& lfObj) {
-        out << lfObj.data;
+        out << *lfObj.data;
         return out;
     }
 };
